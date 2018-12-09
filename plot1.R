d587f83919cd92cd6f110854926fbddac6c7004a
petergajda <petergajda@hotmail.de>
2018-12-09 18:33:09 +0100

    Assignment 1 / Course: Exploratory Data Analysis
    Assignment 1 / Course: Exploratory Data Analysis

---
+++ plot1.R
@@ -0,0 +1,34 @@
+# Assignment 1 / Course: Exploratory Data Analysis
+
+setwd("C:/Desktop")
+library(lubridate)
+
+
+# Getting the data
+powerdata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# Date as type "date"
+powerdata$Date <- as.Date(powerdata$Date, "%d/%m/%Y")
+
+# subset Date between 2007-02-01 and 2007-02-02
+powerdata <- subset(powerdata, powerdata$Date >= as.Date("2007-02-01"))
+powerdata <- subset(powerdata, powerdata$Date <= as.Date("2007-02-02"))
+
+# Combine Date and Time into one column
+powerdata$Datetime <- as.POSIXct(paste(powerdata$Date, powerdata$Time), format="%Y-%m-%d %H:%M:%S")
+
+# Reorder columns and drop Date and Time
+powerdata <- powerdata[,c(10,3:9)]
+
+
+### PLOT 1
+
+png(filename="plot1.png", width = 480, height = 480, units = "px")
+
+hist(powerdata$Global_active_power,
+     col = "red",
+     xlab = "Global Active Power (kilowatts)",
+     ylab = "Frequency",
+     main = "Global Active Power")
+
+dev.off()
---
+++ plot4.R
@@ -0,0 +1,59 @@
+# Assignment 1 / Course: Exploratory Data Analysis
+
+setwd("C:/Desktop")
+library(lubridate)
+
+
+# Getting the data
+powerdata <- read.table("household_power_consumption.txt", header=TRUE, sep=";", na.strings = "?", colClasses = c('character','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+
+# Date as type "date"
+powerdata$Date <- as.Date(powerdata$Date, "%d/%m/%Y")
+
+# subset Date between 2007-02-01 and 2007-02-02
+powerdata <- subset(powerdata, powerdata$Date >= as.Date("2007-02-01"))
+powerdata <- subset(powerdata, powerdata$Date <= as.Date("2007-02-02"))
+
+# Combine Date and Time into one column
+powerdata$Datetime <- as.POSIXct(paste(powerdata$Date, powerdata$Time), format="%Y-%m-%d %H:%M:%S")
+
+# Reorder columns and drop Date and Time
+powerdata <- powerdata[,c(10,3:9)]
+
+
+### PLOT 4
+
+png(filename="plot4.png", width = 480, height = 480, units = "px")
+
+par(mfrow = c(2,2))
+
+#topleft
+plot(powerdata$Datetime, powerdata$Global_active_power,
+     type = "l",
+     ylab = "Global Active Power (kilowatts)",
+     xlab = "")
+
+#topright
+plot(powerdata$Datetime, powerdata$Voltage,
+     type = "l",
+     ylab = "Voltage",
+     xlab = "datetime")
+
+#lowerleft
+plot(powerdata$Datetime, powerdata$Sub_metering_1,
+     type = "l",
+     ylab = "Energy sub metering",
+     xlab = "")
+legend("topright", legend=c("Sub_metering 1", "Sub_metering 2", "Sub_metering 3"), col=c("black","red", "blue"), lty=1, bty = "n")
+lines(powerdata$Datetime, powerdata$Sub_metering_2, col="red")
+lines(powerdata$Datetime, powerdata$Sub_metering_3, col="blue")
+
+#lowerright
+plot(powerdata$Datetime, powerdata$Global_reactive_power,
+     type = "l",
+     lwd = 1,
+     ylab = "Global_reactive_power",
+     xlab = "datetime")
+
+
+dev.off()
